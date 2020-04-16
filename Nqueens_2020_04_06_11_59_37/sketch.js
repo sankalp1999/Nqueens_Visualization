77b6c6cb289fb9044c4f12460a9e343b36c3e4fb
Sankalp Shubham <sankalp.shubham99@gmail.com>
2020-04-17 00:07:25 +0530

    Applied some changes to html and css

--- Nqueens_2020_04_06_11_59_37/sketch.js
+++ Nqueens_2020_04_06_11_59_37/sketch.js
@@ -1,3 +1,11 @@
+/* Written in p5.js (https://p5js.org/)
+ * Under Creative Commons License
+ * https://creativecommons.org/licenses/by-sa/4.0/
+ * Written by Sankalp Shubham, 29-March-2020
+ *
+ */
+
+
 var slider;
 var cols = 8; // figure out how to take input
 var rows = cols;
@@ -6,60 +14,57 @@ var board = [];
 var waiting_time; // Waiting time 
 let monoSynth;
 var steps;
+var cnv;
+var input;
+var button;
 monoSynth = new p5.MonoSynth();
 
 fr = 5; // FrameRate
 let mySound;
 // For sound
 function playSynth() {
-  userStartAudio();
+    userStartAudio();
 
-  let note = random(['D5', 'C5', 'E5', 'A5', 'B5']);
-  // note velocity (volume, from 0 to 1)
-  let velocity = random();
-  // time from now (in seconds)
-  let time = 0;
-  // note duration (in seconds)
-  let dur = 1/6;
+    let note = random(['D5', 'C5', 'E5', 'A5', 'B5']);
+    // note velocity (volume, from 0 to 1)
+    let velocity = random();
+    // time from now (in seconds)
+    let time = 0;
+    // note duration (in seconds)
+    let dur = 1 / 6;
 
-  monoSynth.play(note, velocity, time, dur);
+    // monoSynth.play(note, velocity, time, dur);
 }
 
 
 
 function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('s.wav');
+    soundFormats('mp3', 'ogg');
+    mySound = loadSound('s.wav');
 }
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function is_attacked(board, row, col, n)
-{       
-  await sleep(waiting_time);
-  for (let i = 0; i < n; i++)
-    {
-        if(board[row][i] == 1)
+async function is_attacked(board, row, col, n) {
+
+    await sleep(waiting_time);
+    for (let i = 0; i < n; i++) {
+        if (board[row][i] == 1)
             return true;
     }
-    for (let i = 0; i < n; i++)
-    {
-        if(board[i][col] == 1)
+    for (let i = 0; i < n; i++) {
+        if (board[i][col] == 1)
             return true;
     }
-    for (let p = 0; p < n; p++)
-    {
-        for (let q = 0; q < n; q++)
-        {
-            if(p + q == row + col)
-            {
-                if(board[p][q] == 1)
+    for (let p = 0; p < n; p++) {
+        for (let q = 0; q < n; q++) {
+            if (p + q == row + col) {
+                if (board[p][q] == 1)
                     return true;
             }
-            if(p - q == row - col)
-            {
-                if(board[p][q] == 1)
+            if (p - q == row - col) {
+                if (board[p][q] == 1)
                     return true;
             }
 
@@ -69,28 +74,22 @@ async function is_attacked(board, row, col, n)
 }
 
 // DFS
-async function nqueens(board, N, n)
-{ 
-  await sleep(waiting_time);
-  if (N == 0) 
-    {   
-      return true;
+async function nqueens(board, N, n) {
+    await sleep(waiting_time);
+    if (N == 0) {
+        return true;
     }
 
-    for (let i = 0; i < n; i++)
-    {
-        for (let j = 0; j < n; j++)
-        {
-            if( await is_attacked(board, i, j, n) )
-            {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (await is_attacked(board, i, j, n)) {
                 continue;
             }
             board[i][j] = 1;
-            if(board[i][j] == 1)
-            {
-            playSynth();
+            if (board[i][j] == 1) {
+                playSynth();
             }
-          if (await nqueens(board, N - 1, n))
+            if (await nqueens(board, N - 1, n))
                 return true;
             board[i][j] = 0;
         }
@@ -98,69 +97,102 @@ async function nqueens(board, N, n)
     return false;
 }
 
-function setup() {  
- 
-   // createCanvas(1000,1000);
-   createCanvas(windowWidth, windowHeight);
-   frameRate(10);
-   // Change waiting time using slider
-  
-   slider = createSlider(0, 50, 100);
-   slider.position(100, 700);
-   slider.style('width', '100px');
 
-  for(let i = 0; i < rows ; i++)
-  {  
-    board[i] = [];
-     for(let j = 0; j < cols; j++)
-     {
-       board[i][j] = 0;
-     }
-  }
+// // Resizing
+function windowResized() {
+    cnv = createCanvas(800, 800);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+
+}
+
+
+
+function setup() {
+
+    var cnv = createCanvas(800, 800);
+    // Input taking
+    // input = createInput();
+    // input.position(850, height + 80);
+    background(255, 255, 255);
+    // Button for start
+    button = createButton('Amaze me');
+    button.position(720, height + 80);
+    button.mousePressed(init);
+    button.style('width', '400px');
+    button.style('height', '50px');
+
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+
+    // Change waiting time using slider
+
+
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < cols; j++) {
+            board[i][j] = 0;
+        }
+    }
+}
+
+function init() {
+
+    rows = round(random(4, 8));
+    cols = rows;
+    setup();
     nqueens(board, rows, rows);
-  
+}
+
+function mousePressed() {
+    nqueens(board, rows, rows);
 }
 
 function draw() {
-  waiting_time = slider.value();
-  print(slider.value());
-  for(let i = 0; i < rows ; i++)
-  {
-  
-    for(let j = 0; j < cols ; j++)
-      {
-        let f2 = 200;
-        
-        let factor = rows * 10;
-        let x = i * factor + f2;
-        let y = j * factor + f2/4;
-        stroke(10);
-        if(board[i][j] == 1)
-        {
-          fill(227,38,54);
-          rect(x,y,factor,factor);
-          noFill();
-          fill(255,100,100);
-          circle(x + (factor)/2 , y + factor/2, factor/2);  
-          noFill();
+
+    waiting_time = map(0, mouseY, 0, 50);
+    background(255, 255, 255);
+    textSize(40);
+
+    text("N = " + rows, 300, height - 60);
+
+    for (let i = 0; i < rows; i++) {
+
+        for (let j = 0; j < cols; j++) {
+
+            // rows = (0, 16, 4, 8);
+            factor = rows * 10;
+
+
+            let x = i * factor + 40;
+            let y = j * factor + 40;
+
+            stroke(10);
+            if (board[i][j] == 1) {
+
+                fill(227, 38, 54);
+                rect(x, y, factor, factor);
+                noFill();
+                fill(255, 100, 100);
+                circle(x + (factor) / 2, y + factor / 2, factor / 2);
+                noFill();
+            } else {
+                // Draws the chessboard
+                if ((i + j) % 2 == 0) {
+                    fill(255, 206, 158); // Light
+                } else {
+                    fill(209, 139, 30); // Dark
+                }
+
+                rect(x, y, factor, factor);
+                noFill();
+
+            }
         }
-        else 
-        { 
-          // Draws the chessboard
-          if( (i+j)%2 == 0)
-          {
-            fill(255, 206, 158); // Light
-          }
-          else
-          {
-           fill(209, 139, 30);   // Dark
-          }
-          
-          rect(x,y,factor,factor);
-          noFill();
-          textSize(15);
-        }
-      }
-  }
+    }
+    text("speed", 400, 800);
 
 }
